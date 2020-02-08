b98de1360375fb6e84bb63c93818b910d6a4fdce
ngokcimen <nilgokcimen@gmail.com>
2020-02-08 20:38:42 +0300

    Tests added

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,6 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.*;
+
 /**
  * Unit test for simple App.
  */
@@ -34,5 +36,42 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testEmptyArray()
+    {
+        ArrayList<String> array = new ArrayList<>();
+
+
+    }
+
+    public void testBigQuantity() {
+        ArrayList<String> array = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+        ArrayList<String> nArray = new ArrayList<>(Arrays.asList("2", "3", "4", "5"));
+        int quantity = 5;
+        ArrayList<String> result = App.generate(array, nArray, quantity, 2);
+        assertEquals(null, result);
+
+    }
+
+    public void testNegativeQuantity(){
+        ArrayList<String> array = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+        ArrayList<String> nArray = new ArrayList<>(Arrays.asList("2", "3", "4", "5"));
+        int quantity = -5;
+        ArrayList<String> result = App.generate(array, nArray, quantity, 2);
+        assertEquals(null, result);
+    }
+
+    public void testNumListHasNonDigitValue(){
+        ArrayList<String> nArray = new ArrayList<>(Arrays.asList("2", "b", "4", "5"));
+        boolean result = App.containsNonNumeric(nArray);
+        assertEquals(true, result);
+    }
+
+    public void testWorkingAsExpected(){
+        ArrayList<String> array = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+        ArrayList<String> nArray = new ArrayList<>(Arrays.asList("2", "3", "4", "5"));
+        ArrayList<String> result = App.generate(array, nArray, 3, 2);
+        assertNotNull(result);
     }
 }
